82168f9d41a31e49b937d729bdf55638153b58e1
PieroMS <piero.mendieta6@gmail.com>
2025-05-26 14:59:55 -0500

    dataUser

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,8 +26,11 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'country' => fake()->country(),
+            'age' => fake()->numberBetween(18, 85),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('12345678'),
             'remember_token' => Str::random(10),
         ];
     }
